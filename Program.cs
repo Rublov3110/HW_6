eb272a60f043f1c8bc4282e58353195ad1a67b42
Rublov Viacheslav <timmy31101994@gmail.com>
2022-05-13 19:45:41 +0300

    Start HW_6

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HW_6
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            PCStore pCStore = new PCStore();
+            pCStore.Run();
+
+            Console.ReadKey();
+        }
+    }
+}
